0cc2c9ecbc9cf42a8f5ac829995cfcdab8fabba0
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-09-19 17:57:07 +0530

    Update NovalnetPaymentAbstract.php

--- src/Methods/NovalnetPaymentAbstract.php
+++ src/Methods/NovalnetPaymentAbstract.php
@@ -212,7 +212,6 @@ abstract class NovalnetPaymentAbstract extends PaymentMethodBaseService
     {
         if($orderId > 0) {
             $transactionDetails = $this->paymentService->getDetailsFromPaymentProperty($orderId);
-            $this->getLogger(__METHOD__)->error('methods array', $transactionDetails);
             if( strpos($this::PAYMENT_KEY, 'NOVALNET') !== false &&  ( (!empty($transactionDetails['tx_status']) && !in_array($transactionDetails['tx_status'], ['PENDING', 'ON_HOLD', 'CONFIRMED', 'DEACTIVATED'])) || empty($transactionDetails['tx_status']) )) {
                 return true;
             }
