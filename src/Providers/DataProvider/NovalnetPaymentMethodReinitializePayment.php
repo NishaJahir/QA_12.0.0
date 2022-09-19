8e45649e6b53430521e7bb7d708bfbfb40bbf832
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-09-19 11:20:18 +0530

    Update NovalnetPaymentMethodReinitializePayment.php

--- src/Providers/DataProvider/NovalnetPaymentMethodReinitializePayment.php
+++ src/Providers/DataProvider/NovalnetPaymentMethodReinitializePayment.php
@@ -1,1 +1,29 @@
+<?php
+/**
+ * This module is used for real time processing of
+ * Novalnet payment module of customers.
+ * This free contribution made by request.
+ * 
+ * If you have found this script useful a small
+ * recommendation as well as a comment on merchant form
+ * would be greatly appreciated.
+ *
+ * @author       Novalnet AG
+ * @copyright(C) Novalnet 
+ * All rights reserved. https://www.novalnet.de/payment-plugins/kostenlos/lizenz
+ */
+
+namespace Novalnet\Providers\DataProvider;
+
+use Plenty\Plugin\Templates\Twig;
+
+class NovalnetPaymentMethodReinitializePayment
+{
+	public function call(Twig $twig, $arg)
+	{
+		
+		
+	}
+	
+}
 
