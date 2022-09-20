ecfcb6a28891041af2c5c07fff9b9d31e425f87b
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-09-20 10:45:46 +0530

    Update NovalnetPaymentMethodReinitializePayment.php

--- src/Providers/DataProvider/NovalnetPaymentMethodReinitializePayment.php
+++ src/Providers/DataProvider/NovalnetPaymentMethodReinitializePayment.php
@@ -25,34 +25,6 @@ class NovalnetPaymentMethodReinitializePayment
 {
     public function call(Twig $twig, $arg)
     {
-        $order = $arg[0];
-        $paymentService = pluginApp(PaymentService::class);
-        $basketRepository = pluginApp(BasketRepositoryContract::class);
-        $sessionStorage = pluginApp(FrontendSessionStorageFactoryContract::class);
         
-        
-        // Get the Novalnet payment method Id
-        foreach($order['properties'] as $orderProperty) {
-            if($orderProperty['typeId'] == 3)
-            {
-                $mopId = $orderProperty['value'];
-            }
-        }
-        
-        // Get the Novalnet payment key and MOP Id
-        $transactionDetails = $paymentService->getDetailsFromPaymentProperty($order['id']);
-        
-        
-
-        // If the Novalnet payments are rejected do the reinitialize payment
-        if(strpos($transactionDetails['paymentName'], 'novalnet') !== false &&  ((!empty($transactionDetails['tx_status']) && !in_array($transactionDetails['tx_status'], ['PENDING', 'ON_HOLD', 'CONFIRMED', 'DEACTIVATED'])) || empty($transactionDetails['tx_status']))) {
-            return $twig->render('Novalnet::NovalnetPaymentMethodReinitializePayment', [
-                                'order' => $order,
-                                'paymentMethodId' => $mopId,
-                                'paymentKey' => $transactionDetails['paymentName']
-                                ]);
-        } else {
-            return '';
-        }
     }
 }
